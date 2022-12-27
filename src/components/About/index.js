37cbd3e9e20d4862181bf8f841c06407ddb1d374
MKHLink <[likhonhasankomol@gmail.com]>
2022-12-27 14:59:50 -0500

    able to render About me section

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PFP from "../../Assets/ProfilePic/PFP.jpg";
+
+function About(){
+    return(
+        <section className="my-5">
+            <h1 id="about">About Me</h1>
+            <img src={PFP} className="my-2" style={{width:"100%"}} alt="profile-pic"/>
+            <p>
+                 I am an aspiring software developer currently enrolled in a rigourous coding bootcamp at Columbia Coding Bootcamp.<br />
+                I have attened Borough of Manhattan Community College and 
+                obtained an Associates Degree in Computer Scinece. <br />
+                My hobbies include soccer, video games and exploring diffrent software languages.<br />
+                My dream is to have my own video game studio one day and hence
+                I am building my career around software development.<br />
+                Thank you for taking the time to browse through my page.
+            </p>
+        </section>
+    );
+}
+
+export default About;
